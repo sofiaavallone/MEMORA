0ab93abca3bfaed157d0a1173d63f09d7db44c70
rccs2 <rccs2@cin.ufpe.br>
2026-04-17 12:16:03 -0300

    remoção para incluir nova estrutura
    A estrutura dos arquivos foi modificada de forma grande, eu tirei vários componentes e o github, quando faz a junção, não remove arquivos, apenas altera os que são iguais. Por isso vou remover essa versão e dar upload na nova

--- client/src/mainbar/sendbutton.tsx
+++
@@ -1,8 +0,0 @@
-type sendbutton = {
-    click:React.MouseEventHandler<HTMLButtonElement>,
-}
-
-function SendButton(prop:sendbutton){
-    return(<button onClick={prop.click}>gerar flashcards com IA</button>)
-}
-export default SendButton
